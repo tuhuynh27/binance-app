2cdd3345e0e6ccca373c0a813bdb3f4ae9e69a91
Tu Huynh <tu.huynh@linecorp.com>
2021-05-14 13:59:50 +0700

    hot fix

--- server/main.js
+++ server/main.js
@@ -49,7 +49,7 @@ async function getHoldForAsset(asset, apiKey, apiSecret) {
     })
     const trades = await resp.json()
     let price = 0
-    for (const trade of trades) {
+    for (const trade in trades) {
       const isBuy = trade.isBuyer
       if (isBuy) {
         price = parseFloat(trade.price)
