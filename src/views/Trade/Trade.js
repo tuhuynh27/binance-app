212ef990e2484e6ddb26623a35c8b7f7abdb7ee8
Tu Huynh <tu.huynh@linecorp.com>
2021-05-24 01:50:09 +0700

    add highlight

--- src/views/Trade/Trade.js
+++ src/views/Trade/Trade.js
@@ -1,7 +1,7 @@
 import './Trade.css'
 import React, { useState, useEffect, useCallback } from 'react'
 
-import { Spin, Button } from 'antd';
+import { Spin, Button, Input } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons'
 
 import dayjs from 'dayjs'
@@ -13,6 +13,8 @@ function Trade() {
   const [rate, setRate] = useState({})
   const [info, setInfo] = useState({})
   const [configs, setConfigs] = useState({})
+  const [filterInput, setFilterInput] = useState('')
+  const [filter, setFilter] = useState([])
 
   const loadData = useCallback(async () => {
     const allPromises = [getInfo(), getLogs(), getRate(), getConfig()]
@@ -44,7 +46,10 @@ function Trade() {
       const resp = await fetch('https://trade.tuhuynh.com/trade/logs/get')
       const data = await resp.json()
       data.reverse()
-      setLogs(data)
+      setLogs(oldData => {
+        const newItems = data.filter(e => !oldData.find(x => x.time === e.time))
+        return data.map(e => newItems.find(x => x.time === e.time) ? { ...e, isNew: true } : e)
+      })
     } catch(err) {
       console.log(err)
     }
@@ -73,7 +78,20 @@ function Trade() {
   function calcTxPerSecond() {
     if (!info.startTime || !info.txCount) return 0
     const sec = (new Date().getTime() - info.startTime - 10000) / 1000
-    return (info.txCount / sec).toFixed(2)
+    return (info.txCount / sec * 3).toFixed(2)
+  }
+
+  function changeFilterInput(e) {
+    const val = e.target.value
+    setFilterInput(val)
+    if (!val || !val.length) {
+      setFilter([])
+      return
+    }
+
+    if (!val.length) return
+    const vals = val.split(',')
+    setFilter(vals)
   }
 
   return (
@@ -85,20 +103,31 @@ function Trade() {
           <p>Transactions: {info.txCount || 0} ({calcTxPerSecond()} transactions/s)</p>
           <p>Win/Lose: {rate.win || 0} / {rate.lose || 0} (Win rate: {(rate.win / (rate.win + rate.lose) * 100).toFixed(2)}%)</p>
           <p>Profit: <strong>{info.profit ? info.profit.toFixed(2) : 0}</strong> USDT (<strong>{(info.profit / info.initialBalance * 100).toFixed(2)}</strong>%)</p>
-          <p>Market status: Neutral</p>
+          <p>Market status: <span style={{ fontWeight: 'bold', color: 'red' }}>Downtrend</span></p>
           <p>Auto adjust algorithm: Disabled</p>
           <p>Configs <Button size="small" style={{ marginLeft: '5px' }}>Update</Button></p>
           <ul>
-            <li>Dip Threshold: {configs.dipDownThreshold || 0}%</li>
-            <li>Up Trend Threshold: {configs.dipUpThreshold || 0}%</li>
-            <li>Deny Dip: {configs.denyDipDown || 0}%</li>
-            <li>Start To Buy: {configs.startToBuy || 0}%</li>
+            <li>Down Threshold: {configs.downThreshold || 0}%</li>
+            <li>Up Threshold: {configs.upThreshold || 0}%</li>
+            <li>Deny Threshold: {configs.denyThreshold || 0}%</li>
+            <li>Buy Point: {configs.buyPoint || 0}%</li>
             <li>Stop Loss: {configs.stopLoss|| 0}%</li>
             <li>Take Profit: {configs.takeProfit || 0}%</li>
           </ul>
         </div>
+        <div>
+          <Input
+            value={filterInput}
+            onChange={changeFilterInput}
+            placeholder="Filter currency log by name, EG: BTC,ETH" style={{ marginBottom: '20px' }}/>
+        </div>
         <div className="log-table">
-          {logs.map((e, i) => {
+          {logs.filter(e => {
+            if (!filter || !filter.length) {
+              return e
+            }
+            return filter.includes(e.currency)
+          }).map((e, i) => {
             const l = e.msg
             let status = 'black'
             if (l.startsWith('Stop loss ')) {
@@ -108,7 +137,7 @@ function Trade() {
             }
             const time = dayjs(e.time).format('HH:mm:ss')
             return (
-              <p key={i} style={{ color: status, fontWeight: status === 'red' || status === 'green' ? 'bold' : 'normal' }}>
+              <p key={i} style={{ color: status, background: e.isNew ? '#ffff99' : 'white', fontWeight: status === 'red' || status === 'green' ? 'bold' : 'normal' }}>
                 [{time}] {l}
               </p>)
           })}
